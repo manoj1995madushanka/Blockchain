df506d548878b559f295778360d71068f4583e7a
manoj1995madushanka <madushankamanoj414@gmail.com>
2018-10-11 15:11:15 +0530

    check validity

--- main.js
+++ main.js
@@ -15,6 +15,7 @@ class Block{
     }
     calculateHash(){//this hash will helpful to identify block in the blockchain
         return SHA256(this.index+this.previousHash+this.timestamp+JSON.stringify(this.data)).toString();
+        //stringify converts the JS value to JS object notation
     }
 
 }
@@ -38,4 +39,32 @@ class Blockchain{
         newBlock.hash=newBlock.calculateHash();//calculate hash using data and previous block hash value
         this.chain.push(newBlock);//add hash to the array
     }
+
+    //for verify the integrity
+    isChainValid(){
+        for(let i=1;i<this.chain.length;i++){
+            //i=1 because o is the genesis block
+            const currentBlock=this.chain[i];
+            const previousBlock=this.chain[i-1];
+
+            if(currentBlock.hash !== currentBlock.calculateHash()){
+                return false;
+            }
+            if(currentBlock.previousHash !== previousBlock.hash){
+                return false;
+            }
+            return true;//valid chain
+        }
+    }
 }
+
+let forCheck=new Blockchain();
+forCheck.addBlock(new Block(1,"12-10-2018",{amount : 4}));
+forCheck.addBlock(new Block(2,"12-10-2018",{amount : 10}));
+
+//console.log(JSON.stringify(forCheck,null,4));
+//check validity
+console.log ("Is blockchain valid : "+forCheck.isChainValid());
+
+forCheck.chain[1].data={amount:100};
+console.log ("Is blockchain valid : "+forCheck.isChainValid());
